83c635ba3b020140b801662e4c588664a445c3cd
Tolu Daniels <93607903+ToluDaniels@users.noreply.github.com>
2023-06-29 20:21:13 +0100

    task 1&2

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-var */
+/* eslint-disable no-var */
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
