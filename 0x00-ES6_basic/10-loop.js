eb82e70ce04e3c3401eb00eea5eeebef3e4fe51f
Tolu Daniels <93607903+ToluDaniels@users.noreply.github.com>
2023-06-30 18:24:43 +0100

    updated

--- 0x00-ES6_basic/10-loop.js
+++ 0x00-ES6_basic/10-loop.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const appendedArray = [];
-  for (const index of array) {
-    const value = index;
-    appendedArray.push(appendString + value);
+  // eslint-disable-next-line
+  for (const value of array) {
+    // eslint-disable-next-line
+    array[array.indexOf(value)] = appendString + value;
   }
 
-  return appendedArray;
+  return array;
 }
